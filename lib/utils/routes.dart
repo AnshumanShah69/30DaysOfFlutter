92706fbb50f4e6a179c9767f53582b1b88f7dc2c
AnshumanShah69 <februaryleapyear29@gmail.com>
2023-08-30 20:52:12 +0530

    day 5 login button routes

---
+++ lib/utils/routes.dart
@@ -0,0 +1,5 @@
+class MyRoutes {
+  ///here we have done static memeber variable so we need not create n object everytime like x() dirsectly use it
+  static String loginRoute = "/login";
+  static String homeRoute = "/home";
+}
